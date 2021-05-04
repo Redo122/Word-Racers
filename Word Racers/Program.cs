9b2b76d2ae58e0eae43ef374a9418b95dc1430f6
Redo122 <redo122@outlook.com>
2021-05-04 14:54:07 +1000

    Feature Test v0.1

--- Word Racers/Program.cs
+++ Word Racers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Word_Racers
 {
@@ -6,7 +7,35 @@ namespace Word_Racers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dirs = Directory.GetFiles(@"\packs");
+            string[] packs = { "", "", "", "", "", "", "", "", "", "" };
+            string selectedpack;
+            int countpoint = 0;
+            Random rnd = new Random();
+            Console.WriteLine("╔═╗  ╔══╗  ╔═╗╔═════╗╔═════╗╔═════╗   ╔═════╗╔═════╗╔═════╗╔═════╗╔═════╗╔══════╗\n" +
+                              "║ ║ ╔╝  ╚╗ ║ ║║ ╔═╗ ║║ ╔═╗ ║╚╗ ╔╗ ║   ║ ╔═╗ ║║ ╔═╗ ║║ ╔═══╝║ ╔═══╝║ ╔═╗ ║║ ╔════╝\n" +
+                              "║ ║╔╝ ╔╗ ╚╗║ ║║ ║ ║ ║║ ╚═╝ ║ ║ ║║ ║   ║ ╚═╝ ║║ ║ ║ ║║ ║    ║ ╚══╗ ║ ╚═╝ ║║ ╚════╗\n" +
+                              "║ ╚╝ ╔╝╚╗ ╚╝ ║║ ║ ║ ║║ ╔╗ ╔╝ ║ ║║ ║   ║ ╔╗ ╔╝║ ╚═╝ ║║ ║    ║ ╔══╝ ║ ╔╗ ╔╝╚════╗ ║\n" +
+                              "╚╗  ╔╝  ╚╗  ╔╝║ ╚═╝ ║║ ║║ ╚╗╔╝ ╚╝ ║   ║ ║║ ╚╗║ ╔═╗ ║║ ╚═══╗║ ╚═══╗║ ║║ ╚╗╔════╝ ║\n" +
+                              " ╚══╝    ╚══╝ ╚═════╝╚═╝╚══╝╚═════╝   ╚═╝╚══╝╚═╝ ╚═╝╚═════╝╚═════╝╚═╝╚══╝╚══════╝\n" +
+                              "v0.1\n" +
+                              "FEATURE TEST BUILD\n" +
+                              "Select a word pack:");
+            Console.WriteLine(dirs.Length + " packs found:");
+            foreach (string dir in dirs)
+            {
+                packs[countpoint] = dir;
+                countpoint = countpoint + 1;
+                Console.WriteLine(countpoint + ") " + dir);
+            }
+            int selected = Convert.ToInt16(Console.ReadLine());
+            selectedpack = packs[selected - 1];
+            Console.Clear();
+            Console.WriteLine(selectedpack);
+            string[] words = File.ReadAllLines(selectedpack);
+            Console.WriteLine(words.Length);
+            int selectedword = rnd.Next(words.Length);
+            Console.WriteLine(words[selectedword]);
         }
     }
 }
